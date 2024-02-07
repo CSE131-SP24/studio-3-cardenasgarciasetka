75b2e2a61569c0d9b295f01cb29cbebe66e87c60
xebexcs <xebexcs@localhost>
2024-02-07 14:19:18 -0600

    siever

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,20 @@
+package studio3;
+import java.util.Scanner;
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Give an integer greater than 1");
+		int n = in.nextInt();
+			boolean []div2 = new boolean [n]; {
+			for (int i=0; i < n; i++) {
+				for (int j= i; j <n; j+=i) j= j+i^2; {
+				
+				}
+				}
+	System.out.println(n); 
+			}
+	}
+}
+//my brain is breaking.
